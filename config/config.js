d107049cdd5d33bb9ffb9a2003f4dbb7817e180d
unknown <anusha@cronj.com>
2015-06-24 15:15:55 +0530

    updated config file

--- config/config.js
+++ config/config.js
@@ -10,5 +10,12 @@ module.exports = {
     key: {
         privateKey: '37LvDSm4XvjYOh9Y',
         tokenExpiry: 1 * 30 * 1000 * 60 //1 hour
-    }
+    },
+    email: {
+        username: "anusha@cronj.com",
+        password: "cronjpwd",
+        accountName: "jCatalog",
+        verifyEmailUrl: "verifyMail"
+    },
+    url: "http://localhost:8001/#/"
 };
